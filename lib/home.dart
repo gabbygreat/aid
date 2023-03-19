e85944b1e3b2e2dc2b25460ce0846f1d9062bab4
GabbyGreat <gabrieloranekwu@gmail.com>
2023-03-19 17:31:20 +0100

    :recycle: Fixed the onTap

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tabbar/provider.dart';
 import 'package:tabbar/riverpod_switch.dart';
 import 'animated_switch.dart';
 
